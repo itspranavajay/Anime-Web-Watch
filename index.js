babba48a40614af270c3177293badead38e9daad
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-01-07 21:46:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 var server = http.createServer(app);
 
-app.get("/moezilla", (request, response) => {
-  let moezilla = request.query.url
-  if(!moezilla) return response.send("Something Wrong :/")
+app.get("/mp4", (request, response) => {
+  let mp4 = request.query.moezilla
+  if(!mp4) return response.send("Something Wrong :/")
   response.set('Content-Type', 'text/html');
   response.send(Buffer.from(`<!DOCTYPE html>
 <html>
@@ -21,7 +21,33 @@ video {
 <body style="text-align:center;">
 
 <video width="1000" controls>
- <source src="${moezilla}" type="video/mp4, video/mkv">
+ <source src="${mp4}" type="video/mp4">
+  RedAura Is Best
+</video>
+</body>
+</html>
+`));
+})
+
+app.get("/mkv", (request, response) => {
+  let mkv = request.query.moezilla
+  if(!mkv) return response.send("Something Wrong :/")
+  response.set('Content-Type', 'text/html');
+  response.send(Buffer.from(`<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+video {
+  max-width: 100%;
+  height: auto;
+}
+</style>
+</head>
+<body style="text-align:center;">
+
+<video width="1000" controls>
+ <source src="${mkv}" type="video/mkv">
   RedAura Is Best
 </video>
 </body>
